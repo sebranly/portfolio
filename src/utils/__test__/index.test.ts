e4603fb9ca7b2e9e17d597b2d192a293b2c1c128
sebranly <sebranly.pinf@gmail.com>
2023-01-16 17:08:46 +0100

    test: add some for the utils

--- src/utils/__test__/index.test.ts
+++ src/utils/__test__/index.test.ts
@@ -2,6 +2,7 @@ import {
   alphabetizeTags,
   areFemaleContributors,
   enhanceTags,
+  filterProjectsByTag,
   getAllTags,
   getAllYears,
   getGitHubRepo,
@@ -179,4 +180,36 @@ test('getGitHubRepo', () => {
 
 test('getWebsite', () => {
   expect(getWebsite('dmz')).toBe('https://sebranly.github.io/dmz');
+});
+
+test('filterProjectsByTag', () => {
+  const project1: Project = {
+    description: ['Some description'],
+    title: 'Project 1',
+    subtitle: 'Some subtitle',
+    tags: [Tag.Archived, Tag.GUI],
+    years: [2012]
+  };
+
+  const project2: Project = {
+    description: ['Some description'],
+    title: 'Project 1',
+    subtitle: 'Some subtitle',
+    tags: [Tag.Archived, Tag.CLI],
+    years: [2013]
+  };
+
+  const project3: Project = {
+    description: ['Some description'],
+    title: 'Project 1',
+    subtitle: 'Some subtitle',
+    tags: [Tag.Archived, Tag.Website],
+    years: [2014]
+  };
+
+  expect(filterProjectsByTag([], Tag.All)).toStrictEqual([])
+  expect(filterProjectsByTag([project1, project2, project3], Tag.All)).toStrictEqual([project1, project2, project3])
+  expect(filterProjectsByTag([project1, project2, project3], Tag.GUI)).toStrictEqual([project1])
+  expect(filterProjectsByTag([project1, project2, project3], Tag.CLI)).toStrictEqual([project2])
+  expect(filterProjectsByTag([project1, project2, project3], Tag.Website)).toStrictEqual([project3])
 });
