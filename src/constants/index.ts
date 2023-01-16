ebf6d4169822c91fd7f4fbf4682ecf97ac2de85d
sebranly <sebranly.pinf@gmail.com>
2023-01-16 17:46:05 +0100

    feat: add utils

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export const STEPHANE = 'Stéphane';
 export const TAB_COUNT = 4;
+export const PROJECTS_PER_PAGE = 2;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { Color, Contributor, Project, Tag } from '../types';
 import { flatten, uniq } from 'lodash';
-import { AUTHOR_GITHUB, GITHUB_PAGES_URL } from '../constants/general';
+import { AUTHOR_GITHUB, GITHUB_PAGES_URL, PROJECTS_PER_PAGE } from '../constants/general';
 
 /**
  * @name getGitHubRepo
@@ -234,18 +234,55 @@ const filterProjectsByTag = (projects: Project[], tag: Tag) => {
   return filteredProjects;
 };
 
+/**
+ * @name generatePages
+ * @description Returns all the pages possible for projects
+ */
+const generatePages = (projectsCount: number, projectsPerPage = PROJECTS_PER_PAGE) => {
+  if (projectsCount === 0) return [];
+
+  const rest = projectsCount % projectsPerPage;
+  const areCompletePages = rest === 0;
+  const delta = areCompletePages ? 0 : 1;
+  const maxPage = projectsCount / projectsPerPage + delta;
+
+  return numberRange(1, maxPage);
+}
+
+/**
+ * @name numberRange
+ * @description Returns an array containing all numbers from min to max included
+ */
+const numberRange = (min: number, max: number) => {
+  const numbers = [];
+  for (let i = min; i <= max; i += 1) numbers.push(i);
+  return numbers;
+};
+
+const getProjectsForPage = (projects: Project[], pageNumber: number, projectsPerPage = PROJECTS_PER_PAGE) => {
+  const pageIndex=  pageNumber - 1;
+  const projectsIndexStart = pageIndex * projectsPerPage;
+  const projectsIndexEnd = (pageIndex + 1) * projectsPerPage;
+
+  const projectsForPage = projects.slice(projectsIndexStart, projectsIndexEnd);
+  return projectsForPage;
+};
+
 export {
   alphabetizeTags,
   areFemaleContributors,
   enhanceTags,
   filterProjectsByTag,
+  generatePages,
   getAllTags,
   getAllYears,
   getGitHubRepo,
+  getProjectsForPage,
   getTagColor,
   getTailwindBackgroundColor,
   getTailwindHoverBackgroundColor,
   getWebsite,
   hasTagTranslation,
+  numberRange,
   pluralize
 };
