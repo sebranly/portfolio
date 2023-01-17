bc2657de63a634042bc5cdc89cfa0bfc33f5af3f
sebranly <sebranly.pinf@gmail.com>
2023-01-17 15:10:14 +0100

    test: make project snapshot more complete

--- src/components/__test__/Project.test.tsx
+++ src/components/__test__/Project.test.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { Project as ProjectType, Tag } from '../../types';
+import { Project as ProjectType, Role, Tag } from '../../types';
 import { Project } from '../Project';
 
 test('Project', () => {
   const project: ProjectType = {
+    contributors: [
+      {
+        name: 'projects.people.anonymousPerson',
+        roles: [Role.Developer]
+      }
+    ],
     description: ['Some description', 'And another sentence'],
+    download: 'some download link',
+    github: 'some github link',
+    video: 'some video link',
+    website: 'some website link',
+    textualSnapshot: [
+      '+---+---+---+---+',
+      '| #   # |       |',
+      '+---+   +   +---+',
+      '|     # |   |   |',
+      '+   +   +   +   +',
+      '|   | #   #   # |',
+      '+---+---+---+---+'
+    ],
     title: 'Some project',
     subtitle: 'Some subtitle',
     tags: [Tag.Archived, Tag.JavaScript],
