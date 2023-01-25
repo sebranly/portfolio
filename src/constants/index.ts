00532e6e7f1ba8a3a78b44771bfd824bf3c3b6f9
Sebastien Branly <sebranly.pinf@gmail.com>
2023-01-25 09:52:07 +0100

    Cookie for navigation (#17)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,6 @@
+export const COOKIE_PAGE = 'current-page';
+export const COOKIE_TAB = 'current-tab';
+export const PROJECTS_PER_PAGE = 4;
 export const STEPHANE = 'Stéphane';
 export const TAB_COUNT = 4;
-export const PROJECTS_PER_PAGE = 4;
 export const SCROLL_TIMEOUT = 200;
